9787e00958d4a43b85cb9860c9e95d9516295ca6
suhila77 <suhilab9@gmail.com>
2022-12-30 08:25:40 -0800

    0-isuppe

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,30 @@
 #include "main.h"
-
+#include <stdio.h>
 
 /**
- * rot13 - encoder astring using rot13
- * @str:the string targeted
- * Return: return the encoded string
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ * Return: *s
  */
 
-char *rot13(char *str)
+char *rot13(char *)
 {
-	int index1, index2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'L', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'D', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 
-	while (str[++index1])
+	for (i = 0; s[i] != '0'; j++)
 	{
-		for (index2 = 0; index2 < 52; index2++)
+		for (i= 0; j < 52; j++)
 		{
-			if (str[index1] == alphabet[index2])
+			if (s[i] == data[j])
 			{
-				str[index1] = rot13key[index2];
+				s[i] = datarot[j];
 				break;
-				}
+			}
 		}
 	}
-	return (str);
+	return (s);
 }
