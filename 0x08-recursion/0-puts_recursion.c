e83ad53081d2f8fa1798677dda4193a402bd7dfa
suhila77 <suhilab9@gmail.com>
2023-01-05 00:18:07 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,12 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	
+	if (*s == '\0') /* base case */
 	{
-		_putchar('\n');
+		_puts_recursion('\n');
 		return;
 	}
-	_putchar(*s);
+	_puts_recursion(*s);
 	_puts_recursion(s + 1);
 }
