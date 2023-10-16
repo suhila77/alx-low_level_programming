82c999c45d1db53b90446a1c91cb6a7fd0a82c35
suhila77 <suhilab9@gmail.com>
2023-10-16 12:12:24 -0700

    TAS 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * rest_to_98 - update the vakue of n to 98
+ * reset_to_98 - update the vakue of n to 98
  * @n: input
  * Return: n
  */
 
-void rest_to_98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
