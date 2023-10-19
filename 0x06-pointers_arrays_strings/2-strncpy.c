5c63eb3e039e27aa09423f28edff44e67b9a67c7
suhila77 <suhilab9@gmail.com>
2023-10-19 16:24:26 -0700

    TAS 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,9 +5,9 @@
  * terminating null byte, using at most an inputted number of bytes.
  * if the length of the source string is less than the maximum byte number,
  * the remainder of the destination string is filled whith null bytes.
- * works identially to the standard libaray function 'strncpy'.
- * @dest: buffer storing the string copy 
- * @src: the source string 
+ * works identically to the standard libaray function 'strncpy'.
+ * @dest: buffer storing the string copy
+ * @src: the source string
  * @n: max number of byte copied
  * Return: returns
  */
@@ -16,8 +16,8 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src [i] != '\0'; i++)
-		dest [i] = src[i];
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
 
 	for (; i < n; i++)
 		dest[i] = '\0';
