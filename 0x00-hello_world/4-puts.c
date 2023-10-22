ebed4b4b374be12fb821f7fd61c236185f6a6f73
suhila77 <suhilab9@gmail.com>
2023-10-22 00:24:51 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - A program that prints aline with puts function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"programing is like bulding a multilingual puzzle");
+	return (0);
+}
