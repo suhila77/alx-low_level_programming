2ba1eb915199df521ba3382bd15b126aeb0618e3
suhila77 <suhilab9@gmail.com>
2024-01-17 15:32:18 -0800

    TASK0

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /**
- * main - prints  the minimum number of coins to
+ * main - Prints  the minimum number of coins to
  *        make change for an amount of money.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
  * Return: If the number of arguments is not exactly one - 1.
- *         Otherwise -0.
+ *         Otherwise - 0.
  */
 int main(int argc, char *argv[])
 {
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Hello from 0x0B-malloc_free
