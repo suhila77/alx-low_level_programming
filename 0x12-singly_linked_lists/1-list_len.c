fb2f4d6905fcf356610ee41bf796ee75e3f97ded
suhila77 <suhilab9@gmail.com>
2024-02-01 16:03:08 -0800

    TASKs11

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,4 @@
-#incude "lists.h"
+#include "lists.h"
 
 /**
  * list_len - determines length of linked list
@@ -7,7 +7,7 @@
  * Return: size of list
  */
 
-size_t list_len(const list_t *h);
+size_t list_len(const list_t *h)
 {
 	size_t i = 0;
 
