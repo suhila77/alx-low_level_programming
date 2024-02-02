856b8629476e44b5aa66ee8816f8f25514fe5bc1
suhila77 <suhilab9@gmail.com>
2024-02-01 16:59:33 -0800

    task5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+ * _constructor - executes befor main()
+ *
+ * Return: void
+ */
+
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
