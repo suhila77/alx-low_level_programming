9dbe92339ccd3ae4d82c69aaadfcc56aef05c959
suhila77 <suhilab9@gmail.com>
2024-03-04 17:29:16 -0800

    TASK4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,9 @@ void print_most_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <= 0; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		if (num == 22 || num == 4)
+		if (num == 2 || num == 4)
 			continue;
 		_putchar(num + 48);
 	}
